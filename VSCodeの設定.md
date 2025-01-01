00118b1cde4d7805d57e332ad42ee7bec380624b
LAPTOP-K4QIF604\kenna <kennakaxx@gmail.com>
2025-01-02 01:54:05 +0900

    フォーマット適用

---
+++ VSCodeの設定.md
@@ -0,0 +1,14 @@
+# VSCodeの設定
+
+- VSCodeのSettingを開き、右上の小さなアイコンから`setting.json`を開き、設定を追加する。必要に応じて拡張機能をインストールする。
+```
+    "[javascript]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "[json]": {
+        "editor.defaultFormatter": "vscode.json-language-features"
+    },
+    "[jsonc]": {
+        "editor.defaultFormatter": "vscode.json-language-features"
+    }
+```
--- resource/main.js
+++ resource/main.js
@@ -2,24 +2,24 @@ const express = require("express");
 const app = express();
 
 const server = app.listen(8000, function () {
-  console.log("server started")
-  console.log("port:" + server.address().port)
+  console.log("server started");
+  console.log("port:" + server.address().port);
 });
 
 app.get("/", (req, res) => {
-  console.log("/ is called")
+  console.log("/ is called");
   res.status(200).send("Hello World");
 });
 
 app.get("/test", async function (req, res, next) {
-  console.log("/test is called")
-  res.send("test")
+  console.log("/test is called");
+  res.send("test");
 });
 
 app.get("/test/json", (req, res) => {
-  console.log("/test/json is called")
+  console.log("/test/json is called");
   const data = {
-    message: "test"
+    message: "test",
   };
   res.json(data);
 });
