069464cf875e09012ee12de87e01b57797d72549
LAPTOP-K4QIF604\kenna <kennakaxx@gmail.com>
2025-01-04 17:27:08 +0900

    URLでファイルに直接アクセスできるようにした。これでRPGツクール製ゲームが起動可能

--- resource/express_base.js
+++ resource/express_base.js
@@ -1,8 +1,10 @@
 import express from "express";
+
 const app = express();
 
 //expressの設定
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //bodyでやり取りするデータをObject形式にする
+app.use(express.static("/usr/src/app/game/matching")); //URLでファイルに直接アクセスできるようにする
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
